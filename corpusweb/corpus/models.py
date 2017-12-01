e78b2f6bfd800559bd1e65249dccbe2bcf245937
nschaetti <n.schaetti@gmail.com>
2017-12-01 11:35:31 +0100

    Models

--- corpusweb/corpus/models.py
+++ corpusweb/corpus/models.py
@@ -2,30 +2,107 @@
 from __future__ import unicode_literals
 from django.db import models
 
+
+# A country
+class Country(models.Model):
+    """
+    A country
+    """
+
+    # Properties
+    name = models.CharField(max_length=100)
+
+# end Country
+
+
+# A genre
+class Genre(models.Model):
+    """
+    A Sci-Fi genre
+    """
+
+    # Properties
+    name = models.CharField(max_length=100)
+
+# end Genre
+
+
 # A text
 class Text(models.Model):
+    """
+    A text document
+    """
 
-    title = models.CharField(max_length=100)
+    # Properties
+    average_rating = models.FloatField()
+    classes = models.ForeignKey(Class)
     content = models.TextField(null=True)
-    date = models.DateTimeField(auto_now_add=True, auto_now=False, verbose_name="Publishing date")
+    copyright = models.TextField(null=True)
+    cover = models.ImageField()
+    description = models.TextField(null=True)
+    goodreads = models.CharField(max_length=200)
+    gutenberg = models.CharField(max_length=200)
+    gutenberg_num = models.IntegerField()
+    isbn = models.CharField(max_length=13)
+    language = models.SmallIntegerField(choices=((0, 'English'), (1, 'French'), (2, 'Dutch')))
+    loc_class = models.CharField(max_length=200)
+    original_title = models.CharField(max_length=200)
+    pages = models.IntegerField()
+    plot = models.TextField(null=True)
+    publishing_date = models.DateTimeField(auto_now_add=False, auto_now=False, verbose_name="Publishing date")
+    rating_count = models.IntegerField()
+    summary = models.TextField(null=True)
+    title = models.CharField(max_length=100)
+    type = models.SmallIntegerField(choices=((0, 'Novel'), (1, 'Short Story'), (2, 'Novella')))
+    wikipedia = models.CharField(max_length=200)
+    year = models.IntegerField()
 
+    # To string
     def __str__(self):
-        return self.title
+        """
+        To string
+        :return:
+        """
+        return "{} ({})".format(self.title, self.publishing_date)
     # end __str__
+
 # end Text
 
 
-# An author
-class Author(models.Model):
-
+# A person (author, translator, publisher)
+class Person(models.Model):
+    """
+    A person (author, translator, publisher
+    """
+    # Properties
+    #books = models.ManyToManyField(Text, through='Authorship')
+    born = models.DateField(null=True)
+    bio = models.TextField(null=True)
+    died = models.DateField(null=True)
+    gender = models.PositiveSmallIntegerField(choices=((0, 'Male'), (1, 'Female')))
     name = models.CharField(max_length=30)
-    books = models.ManyToManyField(Text, through='Authorship')
+    summary = models.TextField(null=True)
+    wikipedia = models.CharField(max_length=100)
 
+    # To string
     def __str__(self):
-        return self.name
+        """
+        To string
+        :return:
+        """
+        return "{} ({} - {})".format(self.name, self.born, self.died)
     # end __str__
 
-# end Author
+    # To unicode
+    def __unicode__(self):
+        """
+        To unicode
+        :return:
+        """
+        return u"{} ({} - {})".format(self.name, self.born, self.died)
+    # end __unicode__
+
+# end Person
 
 
 # An authorship
